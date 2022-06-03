14298a3e64b91ce2bbf1447ea47f523e447e751a
eduvlemes <edu.vlemes@gmail.com>
2022-06-03 15:28:34 -0300

    lista de produtos

--- template/js/custom-js/pages.js
+++ template/js/custom-js/pages.js
@@ -1,4 +1,4 @@
 // Add your custom JavaScript for storefront pages here.
-$('#faq-list button').click(function(){
-    $(this).closest('.item').toggleClass('visible')
-})
+// $('#faq-list button').click(function(){
+//     $(this).closest('.item').toggleClass('visible')
+// })
